7934f83cde244ab79ff0b1ea92e382e518cd9211
DezrtS <denzil@walrond.me>
2022-10-21 21:20:57 -0400

    Converted Cars to use Rigidbodies
    Converted to rigibody

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,5 +15,16 @@ public class Player : MonoBehaviour
     {
         transform.position = new Vector3((float)(transform.position.x + Input.GetAxis("Horizontal") / 100), 1, transform.position.z + Input.GetAxis("Vertical") / 100);
         GameObject.Find("Main Camera").transform.position = new Vector3((float)(GameObject.Find("Main Camera").transform.position.x + Input.GetAxis("Horizontal") / 100), 5, GameObject.Find("Main Camera").transform.position.z + Input.GetAxis("Vertical") / 100);
+    
+        
+    
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag == "Vehicle")
+        {
+            transform.position = Vector3.zero;
+        }
     }
 }
