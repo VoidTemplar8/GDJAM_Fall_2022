5190fcf8195c20cd941e0fe2e4f0a5b89b9463e6
DezrtS <denzil@walrond.me>
2022-10-21 20:00:14 -0400

    Added Car
    Scripts and gameobjects

---
+++ Assets/CarController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private int speed;
+    private Vector3 endPosition;
+
+    void Start()
+    {
+        speed = Random.Range(CarSpawner.instance.minSpeed, CarSpawner.instance.maxSpeed);
+        Debug.Log($"Car Speed is {speed}");
+        endPosition = transform.position + transform.forward.normalized * CarSpawner.instance.maxDistance;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, Time.deltaTime * speed);
+    }
+}
