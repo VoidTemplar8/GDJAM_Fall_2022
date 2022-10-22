cda6c95d55dd360853dfec4e9e69457465f76ea7
Random32I <104802424+Random32I@users.noreply.github.com>
2022-10-21 21:38:25 -0400

    Player movement
    Changed the way player moves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,14 +17,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3((float)(transform.position.x + Input.GetAxis("Horizontal") / 100), 1, transform.position.z + Input.GetAxis("Vertical") / 100);
+        transform.position = new Vector3((float)(transform.position.x + Input.GetAxis("Horizontal") / 100), transform.position.y, transform.position.z + Input.GetAxis("Vertical") / 100);
+        GameObject.Find("Main Camera").transform.position = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z - 5);
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.transform.tag == "Vehicle")
+        /*if (collision.transform.tag == "Vehicle")
         {
             transform.position = Vector3.zero;
-        }
+        }*/
     }
 }
