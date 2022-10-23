5d8ce42d0c6b9bc9518fa527ae01624612d23e72
Random32I <104802424+Random32I@users.noreply.github.com>
2022-10-23 13:50:49 -0400

    Death
    added death screen and time limit

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Timer : MonoBehaviour
 {
+    public Text text;
+    public float elapsedTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //you get one minute
+        //Its 11:59 and someone just ordered a pizza for same day delivery, now you must hurry to deliver the pizza. Watch out for cars!
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime += Time.deltaTime;
+        if (60 - Math.Floor(elapsedTime) != 60)
+        {
+            text.text = $"Time: {60 - Math.Floor(elapsedTime)}";
+        }
     }
 }
